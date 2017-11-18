983d1ee2be079052803d4bbe43fab2af8412f7a6
TaverasMisael <macrot29@gmail.com>
2017-11-18 09:41:09 -0400

    feat: make real API calls

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,8 @@
+
+const APIURL = 'http://10.193.0.79:8888'
 export const LoadMarkers = async () => {
   try {
-    const response = await fetch('/mock/marbetes.json')
+    const response = await fetch(`${APIURL}/map`)
     const data = await response.json()
     return data
   } catch (err) {
