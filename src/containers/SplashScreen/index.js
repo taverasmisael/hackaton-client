731148683bd986c97975fa1bc8c04195d663923a
TaverasMisael <macrot29@gmail.com>
2017-11-17 23:01:56 -0400

    feat: add react router and add basic routes

---
+++ src/containers/SplashScreen/index.js
@@ -0,0 +1,8 @@
+import React, { PureComponent } from 'react'
+
+export default class SplashScreen extends PureComponent {
+  state = {}
+  render() {
+    return <p>Lol</p>
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import 'mapbox-gl/dist/mapbox-gl.css';
-import App from '@containers/App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import './index.css'
+import 'mapbox-gl/dist/mapbox-gl.css'
+
+import App from '@containers/App'
+import Login from '@containers/Login'
+import SplashScreen from '@containers/SplashScreen'
+
+import registerServiceWorker from './registerServiceWorker'
+
+ReactDOM.render(
+  <Router>
+    <div>
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/map" component={App} />
+      <Route exact path="/" component={SplashScreen} />
+    </div>
+  </Router>,
+  document.getElementById('root')
+)
+registerServiceWorker()
